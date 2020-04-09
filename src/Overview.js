23c0d1d9968c488781bab3c5afa608ba70df2cc7
Ed Brannin <edbrannin@gmail.com>
2020-04-09 17:10:52 -0400

    Support Mysteries of the Day

--- src/Overview.js
+++ src/Overview.js
@@ -1,14 +1,47 @@
 import React from 'react'
+import useLocalstorage from "@rooks/use-localstorage";
 import Button from './Button'
+import moment from 'moment-easter'
 import { Link } from "@reach/router"
 import useCurrentDay from './currentDay'
 import LinkButton from './LinkButton'
 
 import LATIN_MYSTERY_NAMES from '../data/mystery-links.json'
 
+const getMysteries = (mysteriesByWeekday, { todayMysteries }) => {
+  if (!mysteriesByWeekday) {
+    return todayMysteries;
+  }
+
+  // Weekday
+  const weekday = moment().day()
+  if (weekday === 1 || weekday === 6) {
+    return 'Joyful'
+  }
+  if (weekday === 4) {
+    return 'Luminous';
+  }
+  if (weekday === 2 || weekday === 5) {
+    return 'Sorrowful'
+  }
+  if (weekday === 0 || weekday === 3) {
+    return 'Glorious';
+  }
+
+  return 'UNKNOWN';
+}
+
+const useMysteries = ({ todayMysteries }) => {
+  const { value: mysteriesByWeekday, set: setMysteriesByWeekday } = useLocalstorage("mysteries-by-weekday", false);
+  const actualMysteries = getMysteries(mysteriesByWeekday, { todayMysteries });
+  const toggleMysteriesByWeekday = () => setMysteriesByWeekday(!mysteriesByWeekday);
+  return { actualMysteries, mysteriesByWeekday, toggleMysteriesByWeekday };
+}
+
 const Overview = () => {
   const { currentDay, decrementDay, incrementDay, todayIntention, todayMysteries, targetCurrentDay } = useCurrentDay()
-  const latinMysteryName = LATIN_MYSTERY_NAMES[todayMysteries.toLowerCase()].toLowerCase()
+  const { actualMysteries, mysteriesByWeekday, toggleMysteriesByWeekday } = useMysteries({ todayMysteries });
+  const latinMysteryName = LATIN_MYSTERY_NAMES[actualMysteries.toLowerCase()].toLowerCase()
   const behind = currentDay < targetCurrentDay
   const targetPerDay = Math.round((54 - currentDay) * 10 / (54 - targetCurrentDay)) / 10
   return (
@@ -38,11 +71,11 @@ const Overview = () => {
           Next Day &rarr;
         </Button>
       </div>
-      <h3>Mysteries: {todayMysteries}</h3>
+      <h3>Mysteries: {actualMysteries}</h3>
       <h3>Intention type: {todayIntention}</h3>
       <h2>Start</h2>
-      <LinkButton to={`/pray/${todayMysteries}`}>Show Mysteries</LinkButton>
-      <LinkButton to={`/pray/${todayMysteries}/1`}>Start</LinkButton>
+      <LinkButton to={`/pray/${actualMysteries}`}>Show Mysteries</LinkButton>
+      <LinkButton to={`/pray/${actualMysteries}/1`}>Start</LinkButton>
       <a href={`rosary://${latinMysteryName}`} style={{
         color: 'white',
         textDecoration: 'none',
@@ -51,6 +84,9 @@ const Overview = () => {
           Launch in Rosary app
         </Button>
       </a>
+      <Button onClick={toggleMysteriesByWeekday}>
+        Mystery mode: {mysteriesByWeekday ? 'By Weekday' : 'Round Robin'}
+      </Button>
     </div>
   )
 }
