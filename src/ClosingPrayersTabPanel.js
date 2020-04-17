1782fc06c283d6d8bc14b7b387d82f387c4341f0
Ed Brannin <edbrannin@gmail.com>
2020-04-17 16:40:41 -0400

    Remember closing-prayer style (singular/plural)

--- src/ClosingPrayersTabPanel.js
+++ src/ClosingPrayersTabPanel.js
@@ -1,21 +1,25 @@
 import React from 'react'
+import useLocalstorage from "@rooks/use-localstorage";
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import CLOSING_PRAYERS from '../data/closing_prayer'
 
-const ClosingPrayersTabPanel = ({ todayIntention }) => (
-  <Tabs>
-    <TabList>
-      <Tab>Singular</Tab>
-      <Tab>Plural</Tab>
-    </TabList>
-    <TabPanel>
-      {CLOSING_PRAYERS[todayIntention].Singular}
-    </TabPanel>
-    <TabPanel>
-      {CLOSING_PRAYERS[todayIntention].Plural}
-    </TabPanel>
-  </Tabs>
-)
+const ClosingPrayersTabPanel = ({ todayIntention }) =>{
+  const { set: setDefaultIndex, value: defaultIndex } = useLocalstorage('closing-prayer-tab-index', 0);
+  return (
+    <Tabs defaultIndex={defaultIndex} onSelect={index => setDefaultIndex(index)}>
+      <TabList>
+        <Tab>Singular</Tab>
+        <Tab>Plural</Tab>
+      </TabList>
+      <TabPanel>
+        {CLOSING_PRAYERS[todayIntention].Singular}
+      </TabPanel>
+      <TabPanel>
+        {CLOSING_PRAYERS[todayIntention].Plural}
+      </TabPanel>
+    </Tabs>
+  )
+};
 
 export default ClosingPrayersTabPanel
