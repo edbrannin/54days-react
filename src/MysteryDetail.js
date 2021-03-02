6b75856d5a80f0b323c326fd7257b48115889715
Ed Brannin <edbrannin@gmail.com>
2021-03-01 23:40:52 -0500

    style tweak

--- src/MysteryDetail.js
+++ src/MysteryDetail.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Name = styled.h3`
+  font-weight: bold;
+  margin-top: 0;
+`;
 
 const MysteryDetail = ({ name, fruit, reflection }) => (
   <div>
-    <h3 style={{
-      fontWeight: 'bold',
-    }}>The {name}</h3>
+    <Name>The {name}</Name>
     {/* Markdown, SmartyPants for refelction? */}
     <p>{reflection}</p>
     <p style={{ fontStyle: 'italic', }}>Fruit of the mystery: {fruit}</p>
